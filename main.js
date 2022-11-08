110df7ebcf99d0ea200381559adf2d01c2f8ae4e
pdemianowicz <pdemianowicz@gmail.com>
2022-11-08 16:52:40 +0100

    quick fix

--- main.js
+++ main.js
@@ -14,30 +14,30 @@ navToggle.addEventListener("click", () => {
 });
 
 // ---------- Slider -------------
-const slider = document.querySelector(".slider-container");
-let isDown = false;
-let startX;
-let scrollLeft;
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  freeMode: {
+    enabled: false,
+  },
+  pagination: {
+    enabled: true,
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 
-slider.addEventListener("mousedown", (e) => {
-  isDown = true;
-  slider.classList.add("active");
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-});
-slider.addEventListener("mouseleave", (_) => {
-  isDown = false;
-  slider.classList.remove("active");
-});
-slider.addEventListener("mouseup", (_) => {
-  isDown = false;
-  slider.classList.remove("active");
-});
-slider.addEventListener("mousemove", (e) => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const SCROLL_SPEED = 3;
-  const walk = (x - startX) * SCROLL_SPEED;
-  slider.scrollLeft = scrollLeft - walk;
+  paginationDisabledClass: "swiper-pagination-disabled",
+  breakpoints: {
+    768: {
+      slidesPerView: "auto",
+      spaceBetween: 40,
+      pagination: {
+        enabled: false,
+      },
+
+      freeMode: {
+        enabled: true,
+      },
+    },
+  },
 });
